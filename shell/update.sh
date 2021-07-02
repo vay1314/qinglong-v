33d30e75df3ca682d879035bf19edce8e6055ded
VAY冬冬 <niuniuhaogou@gmail.com>
2021-07-02 23:19:15 +0800

    更新静态资源地址

--- shell/update.sh
+++ shell/update.sh
@@ -254,7 +254,7 @@ update_qinglong() {
     local no_restart="$1"
     echo -e "--------------------------------------------------------------\n"
     [ -f $dir_root/package.json ] && ql_depend_old=$(cat $dir_root/package.json)
-    reset_romote_url ${dir_root} "${github_proxy_url}https://github.com/whyour/qinglong.git"
+    reset_romote_url ${dir_root} "${github_proxy_url}https://github.com/vay1314/qinglong-v.git"
     git_pull_scripts $dir_root
 
     if [[ $exit_status -eq 0 ]]; then
@@ -269,7 +269,7 @@ update_qinglong() {
         echo -e "\n更新$dir_root失败，请检查原因...\n"
     fi
 
-    local url="${github_proxy_url}https://github.com/whyour/qinglong-static.git"
+    local url="${github_proxy_url}https://github.com/vay1314/qinglong-v-static.git"
     if [ -d ${ql_static_repo}/.git ]; then
         reset_romote_url ${ql_static_repo} ${url}
         cd ${ql_static_repo}
