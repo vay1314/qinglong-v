dec68eabf6fc28aa36eb08b944c7c8de1cc358c7
VAY冬冬 <niuniuhaogou@gmail.com>
2021-07-15 09:34:09 +0800

    更新 docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,6 +10,7 @@ mkdir -p /ql/scripts/node_modules
 echo -e "==========Canvas依赖未安装，首次编译安装可能稍慢，请耐心等待==========\n"
 echo -e "==============具体安装时间与机器配置以及网速有关==============\n"
 apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd /ql/scripts && npm install canvas --build-from-source
+cd /ql
 fi
 
 echo -e "======================1. 检测配置文件========================\n"
