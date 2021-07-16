0ffac7f8d8b0033f017c8ff3d381653f1f52e34c
VAY冬冬 <niuniuhaogou@gmail.com>
2021-07-16 13:34:33 +0800

    Dockerfile调整

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,14 +5,6 @@ dir_shell=/ql/shell
 . $dir_shell/share.sh
 link_shell
 
-if [ ! -d "/ql/scripts/node_modules/canvas/" ]; then
-mkdir -p /ql/scripts/node_modules
-echo -e "==========Canvas依赖未安装，首次编译安装可能稍慢，请耐心等待==========\n"
-echo -e "==============具体安装时间与机器配置以及网速有关==============\n"
-apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd /ql/scripts && npm install canvas --build-from-source
-cd /ql
-fi
-
 echo -e "======================1. 检测配置文件========================\n"
 fix_config
 cp -fv $dir_root/docker/front.conf /etc/nginx/conf.d/front.conf
